6fad1f21bc5bbb34d26b2de8329a333d76ea0579
1F10240209 <s1F102402091@iniad.org>
2025-06-26 13:50:26 +0900

    Taskモデルにメソッドを追加

--- todo/models.py
+++ todo/models.py
@@ -7,3 +7,8 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)  # 完了しているか.
     posted_at = models.DateTimeField(default=timezone.now)  # 登録日.
     due_at = models.DateTimeField(null=True, blank=True)  # 締切.
+
+    def is_overdue(self, dt):
+        if self.due_at is None:
+            return False
+        return self.due_at < dt
